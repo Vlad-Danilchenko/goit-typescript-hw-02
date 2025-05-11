ca3b40b71b96d3b38845d0e74bf070d37499bd64
Freasy-Vlad <104465507+Vlad-Danilchenko@users.noreply.github.com>
2025-05-11 15:43:51 +0300

    add

--- src/baseTypes/6.ts
+++ src/baseTypes/6.ts
@@ -2,16 +2,18 @@
   Як ви вкажете типи для аргументів і значень цих функцій, що повертаються?
 */
 
-function showMessage(message) {
+function showMessage(message: string) {
   console.log(message);
 }
 
-function calc(num1, num2) {
+function calc(num1: number, num2: number): number {
   return num1 + num2;
 }
 
 function customError() {
-  throw new Error('Error');
+  throw new Error("Error");
 }
 
 export {};
+
+showMessage("whoo");
